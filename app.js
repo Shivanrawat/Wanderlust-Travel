d2d557b5f01296d3cd98bd179c65a3e0d80a0ce5
Delta Student <Deltastudent@mail.com>
2025-08-10 18:13:34 +0530

    app.js chnage

--- app.js
+++ app.js
@@ -67,9 +67,9 @@ const sessionOptions =  {
   },
 };
 
-// app.get("/" , (req ,res)=>{
-//     res.send("hi , i am root");
-// });
+app.get("/" , (req ,res)=>{
+    res.send("/listings");
+});
 
 
 
